9500816095e639beded015678cafc9eb3147161a
Soroush Kaviani <90821955+srkaviani@users.noreply.github.com>
2024-09-03 16:09:17 +0330

    Update fixer.sh

--- fixer.sh
+++ fixer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 wget https://github.com/srkaviani/JetBackup/raw/main/jetbackup5-base-5.3.9-126_amd64.deb
 dpkg -i jetbackup5-base-5.3.9-126_amd64.deb
-wget https://github.com/srkaviani/JetBackup/blob/main/jetbackup5-cpanel-5.3.9-126_amd64.deb
+wget https://repo.jetlicense.com/ubuntu/dists/focal/stable/main/binary-amd64/jetbackup5-cpanel-5.3.9-126_amd64.deb
 dpkg -i jetbackup5-cpanel-5.3.9-126_amd64.deb
+update_jetbackup
+service jetbackup5d status
