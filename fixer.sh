5bdb9940e93e31d2403ed08d8bc1e2d553452d77
root <root@server.azqeshm.com>
2024-09-03 15:58:15 +0330

    first version

--- fixer.sh
+++ fixer.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+wget https://github.com/srkaviani/JetBackup/raw/main/jetbackup5-base-5.3.9-126_amd64.deb
+dpkg -i jetbackup5-base-5.3.9-126_amd64.deb
+wget https://github.com/srkaviani/JetBackup/blob/main/jetbackup5-cpanel-5.3.9-126_amd64.deb
+dpkg -i jetbackup5-cpanel-5.3.9-126_amd64.deb
