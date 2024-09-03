4226e4b4fc125371c9b71cb90fc4458dd7417e04
Soroush Kaviani <90821955+srkaviani@users.noreply.github.com>
2024-09-03 16:15:13 +0330

    Create fixer_da.sh

---
+++ fixer_da.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget https://github.com/srkaviani/JetBackup/raw/main/jetbackup5-base-5.3.9-126_amd64.deb
+dpkg -i jetbackup5-base-5.3.9-126_amd64.deb
+wget https://repo.jetlicense.com/ubuntu/dists/focal/stable/main/binary-amd64/jetbackup5-directadmin-5.3.9-126_amd64.deb
+dpkg -i jetbackup5-directadmin-5.3.9-126_amd64.deb
+update_jetbackup
+service jetbackup5d status
